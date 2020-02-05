03e27aa8c6505ed3d684b3373ad274ce41344bff
Bennobear <trudelbenjamin@yahoo.de>
2020-02-05 11:53:37 +0100

    Units & Tower - definition

---
+++ Assets/Prefabs/Tower.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [Header("Basics")]
+    public int cost;
+    public int damage;
+    public float range;
+    public float attackSpeed;
+    public float horror;
+    [Header("Utility")]
+    public Transform target;
+    public string enemyTag = "Enemy";
+    public bool canAttackFlying;
+    public bool canAttackInvisible;
+    //miscellaneous effects like Splash Attack / Slow 
+    [Header("Upgrades")]
+    public List<Upgrades> upgrades;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            } 
+        }
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+            return;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    [Header("Basics")]
+    public float movementSpeed;
+    public float life;
+    public float damage;
+    public int reward;
+    [Header("Utility")]
+    public float holy;
+    public bool isFlying;
+    public bool isInvisible;
+    //miscellaneous effects
+    [Header("Versusmode")]
+    public int cost;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+    }
+
+    
+}
