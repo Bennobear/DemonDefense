04293e917569e9bf776727a6653d107ef67504a6
Bennobear <trudelbenjamin@yahoo.de>
2020-02-05 09:44:48 +0100

    Testing

--- Assets/Prefabs/Unit.cs
+++ Assets/Prefabs/Unit.cs
@@ -2,20 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class Unit : MonoBehaviour
 {
-    public float Speed;
+    public float 
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    
-    
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * Speed * Time.deltaTime);
+        
     }
 }
