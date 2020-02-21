cecd92d392d09f894c6d64816cec9251fdc479f7
Bennobear <trudelbenjamin@yahoo.de>
2020-02-21 08:17:44 +0100

    Tower UI and Tower Selection with Placement

---
+++ Assets/Scripts/TestOurTile.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TestOurTile : MonoBehaviour
+{
+	public static TestOurTile instance;
+
+	private WorldTile _tile;
+	// TESTING
+	public GameObject firstTower;
+	public GameObject secondTower;
+	public GameObject thirdTower;
+	private GameObject selectedTower;
+	Shop shop;
+
+	private void Awake()
+	{
+		if (instance != null)
+		{
+			Debug.LogError("More than one BuildManager in Scene!");
+			return;
+		}
+		instance = this;
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (selectedTower != null)
+			{
+				Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				var worldPoint = new Vector3Int(Mathf.FloorToInt(point.x), Mathf.FloorToInt(point.y), 0);
+
+				var tiles = GameTiles.instance.tiles; // This is our Dictionary of tiles
+													  // Playing Tower and Color ground
+
+				if (tiles.TryGetValue(worldPoint, out _tile))
+				{
+					print("Tile " + _tile.Name + " costs: " + _tile.Cost);
+					_tile.TilemapMember.SetTileFlags(_tile.LocalPlace, TileFlags.None);
+					_tile.TilemapMember.SetColor(_tile.LocalPlace, Color.green);
+					Vector3 towerPos = _tile.WorldLocation;
+					towerPos.z = -1;
+					towerPos.x = towerPos.x + 0.5f;
+					towerPos.y = towerPos.y + 0.5f;
+					if (_tile.Blocked)
+					{
+						print("Tile already used");
+					}
+					else
+						Instantiate(selectedTower, towerPos, Quaternion.identity);
+					_tile.Blocked = true;
+
+				}
+			}
+		}
+		else
+		{
+			Debug.Log("No Turret Selected");
+		}
+	}
+	public void SetTurretToBuild(GameObject _turret)
+	{
+		selectedTower = _turret;
+	}
+
+	public void GetTurretToBuild(GameObject _turret)
+	{
+		_turret = selectedTower;
+	}
+}
---
+++ Assets/Shop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    TestOurTile gm;
+
+    public void Start()
+    {
+        gm = TestOurTile.instance;
+    }
+
+    public void PurchaseFirstTower()
+    {
+        Debug.Log("First");
+        gm.SetTurretToBuild(gm.firstTower);
+    }
+
+    public void PurchaseSecondTower()
+    {
+        Debug.Log("Second");
+        gm.SetTurretToBuild(gm.secondTower);
+    }
+
+    public void PurchaseThirdTower()
+    {
+        Debug.Log("Third");
+        gm.SetTurretToBuild(gm.thirdTower);
+    }
+
+
+}
