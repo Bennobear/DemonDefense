06f1b9d918d4eebfb08d0d19017e1412db3b9be1
MaximilianPegasus <maximilian.stuebing@ibs-banking.com>
2020-03-05 07:48:48 +0100

    Healthbar implemented

---
+++ Assets/Scripts/HealthbarEffect.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CodeMonkey.Utils;
+using CodeMonkey;
+
+public class HealthbarEffect : MonoBehaviour {
+
+    private const float DAMAGED_HEALTH_SHRINK_TIMER_MAX = .6f;
+
+    private Image barImage;
+    private Image damagedBarImage;
+    private HealthbarSystem healthSystem;
+    private float damagedHealthShrinkTimer;
+
+    private void Awake()
+    {
+        barImage = transform.Find("healthbar").GetComponent<Image>();
+        damagedBarImage = transform.Find("healthbarshrink").GetComponent<Image>();
+
+    }
+
+    private void Start()
+    {
+        healthSystem = new HealthbarSystem(100);
+        SetHealth(healthSystem.GetHealthNormalized());
+        damagedBarImage.fillAmount = barImage.fillAmount;
+
+        healthSystem.OnDamage += HealthSystem_OnDamage;
+        healthSystem.OnHeal += HealthSystem_OnHeal;
+
+        CMDebug.ButtonUI(new Vector2(-100, -50), "Damage", () => healthSystem.damageHealth(10));
+        CMDebug.ButtonUI(new Vector2(+100, -50), "Heal", () => healthSystem.healHealth(10));
+
+
+    }
+
+    private void Update()
+    {
+        damagedHealthShrinkTimer -= Time.deltaTime;
+        if(damagedHealthShrinkTimer < 0)
+        {
+           if(barImage.fillAmount < damagedBarImage.fillAmount)
+            {
+                float shrinkspeed = 1f;
+                damagedBarImage.fillAmount -= shrinkspeed * Time.deltaTime;
+            }
+        }
+    }
+
+    private void HealthSystem_OnDamage(object sender, System.EventArgs e)
+    {
+        damagedHealthShrinkTimer = DAMAGED_HEALTH_SHRINK_TIMER_MAX;
+        SetHealth(healthSystem.GetHealthNormalized());
+    }
+
+    private void HealthSystem_OnHeal(object sender, System.EventArgs e)
+    {
+        SetHealth(healthSystem.GetHealthNormalized());
+        damagedBarImage.fillAmount = barImage.fillAmount;
+    }
+
+ private void SetHealth(float healthNormalized)
+    {
+        barImage.fillAmount = healthNormalized;
+    }
+
+}
---
+++ Assets/Scripts/HealthbarSystem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthbarSystem : MonoBehaviour
+{
+    public event EventHandler OnDamage;
+    public event EventHandler OnHeal;
+
+    private int healthAmount;
+    private int healthAmountMax;
+
+    public HealthbarSystem(int healthAmount)
+    {
+        healthAmountMax = healthAmount;
+        this.healthAmount = healthAmount;
+    }
+
+    public void damageHealth(int amount)
+    {
+        healthAmount -= amount;
+        if(healthAmount < 0){
+            healthAmount = 0;
+        }
+        if (OnDamage != null) OnDamage(this, EventArgs.Empty);
+    }
+
+    public void healHealth(int amount)
+    {
+        healthAmount += amount;
+        if(healthAmount > healthAmountMax){
+           healthAmount = healthAmountMax;
+        }
+        if (OnHeal != null) OnHeal(this, EventArgs.Empty);
+    }
+
+    public float GetHealthNormalized()
+    {
+        return (float)healthAmount / healthAmountMax;
+    }
+}
