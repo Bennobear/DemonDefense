1f06afe3a9f67f377bd80799a96e595f07b8ab62
MaximilianPegasus <maximilian.stuebing@ibs-banking.com>
2020-03-06 11:12:51 +0100

    Damage Pop-up implemented

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+
+public class Testing : MonoBehaviour
+{    private void Start()
+    {
+        //DamagePopUp.Create(Vector3.zero, 250);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            bool critHit = Random.Range(0, 100) < 30;
+            DamagePopUp.Create(UtilsClass.GetMouseWorldPosition(), 100, critHit);
+        }
+    }
+
+}
