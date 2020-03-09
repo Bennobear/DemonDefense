f412c35b384eb5bfb430520544851501df165fa3
Bennobear <trudelbenjamin@yahoo.de>
2020-03-09 21:08:59 +0100

    Speed Up & Pause Menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject gameOverUI;
     public static bool gameIsOver;
     public GameObject completeLevelUI;
+    public GameObject pauseUI;
+
+    public SceneFader fader;
 
 
     // Start is called before the first frame update
@@ -24,18 +28,60 @@ public class GameManager : MonoBehaviour
         {
             EndGame();
         }
+        
     }
 
-    void EndGame()
+    public void EndGame()
     {
         gameIsOver = true;
         gameOverUI.SetActive(true);
     }
 
-    void WinLevel()
+    public void WinLevel()
     {
         gameIsOver = true;
         completeLevelUI.SetActive(true);
     }
 
+    public void Speedup()
+    {
+        Time.timeScale = 5f;
+        StartCoroutine(Wait());
+    }
+
+    public void PauseGame()
+    {
+        Toggle();
+    }
+
+    public void RetryGame()
+    {
+        Toggle();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu()
+    {
+        Toggle();
+        fader.FadeTo("MainMenu");
+    }
+
+    public void Toggle()
+    {
+        pauseUI.SetActive(!pauseUI.activeSelf);
+        if (pauseUI.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(15f);
+        Time.timeScale = 1f;
+    }
 }
